54426382725c6a989cbb1f3d9819a2ab3ce9b26b
Andrés Giraldo <31672123+agiraldodev@users.noreply.github.com>
2024-04-06 11:41:25 -0500

    Formulario clientes

--- src/views/ClientesView.vue
+++ src/views/ClientesView.vue
@@ -1,8 +1,42 @@
 <script setup>
-  import BarraNavegacion from '@/components/BarraNavegacion.vue';
+import BarraNavegacion from '@/components/BarraNavegacion.vue';
 </script>
 
 <template>
   <BarraNavegacion />
   <h1>Listado de clientes</h1>
+
+  <div class="container">
+    <div class="row">
+      <div class="col-md-6">
+        Lista clientes
+      </div>
+      <div class="col-md-6 bg-warning p-2">
+        <h3>Crear cliente</h3>
+        <form>
+          <div class="form-group">
+            <label for="nombres">Nombres</label>
+            <input type="text" class="form-control" id="nombres">
+          </div>
+          <div class="form-group">
+            <label for="apellidos">Apellidos</label>
+            <input type="text" class="form-control" id="apellidos">
+          </div>
+          <div class="form-group">
+            <label for="cedula">Cédula</label>
+            <input type="number" class="form-control" id="cedula">
+          </div>
+          <div class="form-group">
+            <label for="telefono">Teléfono</label>
+            <input type="number" class="form-control" id="telefono">
+          </div>
+          <div class="form-group">
+            <label for="email">Correo</label>
+            <input type="email" class="form-control" id="email">
+          </div>
+          <button class="btn btn-success w-100 mt-2">Registrar Cliente</button>
+        </form>
+      </div>
+    </div>
+  </div>
 </template>
